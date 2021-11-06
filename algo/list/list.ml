5e915321c767d931775818b09577b53409f88adc
ChloeLucas1 <90566955+ChloeLucas1@users.noreply.github.com>
2021-11-06 19:27:45 +0100

    Create list.ml

---
+++ algo/list/list.ml
@@ -0,0 +1,19 @@
+(* This file contains list algorithms *)
+
+(** [find x l] search an element x in a list l *)
+let rec find x l = match l with
+    | [] -> false
+    | e::q -> e = x || find x q
+
+(** [reverse l] reverse a list l *)
+let reverse l =
+    let rec rev l acc = match l with
+        | [] -> acc
+        | e::q -> rev q (e::acc) in
+    rev l []
+
+(** [increasing l] return if the list is increasing or not *)
+let rec increasing l = match l with
+    | [] -> true
+    | [e] -> true
+    | e1::e2::q -> e1 <= e2 && increasing (e2::q)
