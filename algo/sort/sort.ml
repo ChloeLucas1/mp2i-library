6d332b5e062a9ae4c36d36fe01af0f53d1e29a2c
ChloeLucas1 <90566955+ChloeLucas1@users.noreply.github.com>
2021-11-06 20:31:18 +0100

    Update sort.ml

--- algo/sort/sort.ml
+++ algo/sort/sort.ml
@@ -1,4 +1,4 @@
-(* This file contains searching algorithms *)
+(* This file contains sorting algorithms *)
 
 (** [swap t i j] exchanges [t.(i)] and [t.(j)] *)
 let swap t i j =
@@ -13,3 +13,24 @@ let sort_bubble t =
       if t.(j) > t.(j + 1) then swap t j (j + 1)
       done
   done;;
+
+(** [split l] splits a list l into two lists *)
+let rec split l = match l with
+    | [] -> [], []
+    | [e] -> [e], []
+    | e1::e2::q -> let l1, l2 = split q in
+                   (e1::l1), (e2::l2)
+
+(** [fusion l1 l2] merges two sorted lists into one sorted list *)
+let rec fusion l1 l2 = match l1, l2 with
+    | [], _ -> l2
+    | _, [] -> l1
+    | e1::q1, e2::q2 when e1 <= e2 -> e1::(fusion q1 l2)
+    | e1::q1, e2::q2 -> e2::(fusion l1 q2)
+
+(** [merge_sort] sorts a list l *)
+let rec merge_sort l = match l with
+    | [] -> []
+    | [e] -> [e]
+    | l -> let l1, l2 = split l in
+           fusion (merge_sort l1) (merge_sort l2)
