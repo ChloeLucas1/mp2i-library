fbf576f5ec008f3036eb13621898b0d0ec71a6f4
ChloeLucas1 <90566955+ChloeLucas1@users.noreply.github.com>
2021-11-07 15:58:50 +0100

    Update queue.ml

--- ds/queue/queue.ml
+++ ds/queue/queue.ml
@@ -6,7 +6,8 @@ let queue_empty f = f.l1 = [] && f.l2 = [];;
 (** [queue_add f e] adds an element e to a queue f *)
 let queue_add f e = {l1 = e::f.l1; l2 = f.l2};;
 
-(** [queue_pop f] deletes and returns the first element of a queue f *)
+(** [queue_pop f] returns the first element of a queue f and the queue without e *)
 let rec queue_pop f = match f.l1 with
 | e::q -> e, {l1 = q; l2 = f.l2}
+| [] when f.l2 = [] -> failwith "empty queue"
 | [] -> queue_pop {l1 = List.rev f.l2; l2 = []};;
