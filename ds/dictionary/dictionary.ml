e9622b76e59b58c1e9262b752c54926639a454c2
ChloeLucas1 <90566955+ChloeLucas1@users.noreply.github.com>
2021-11-07 17:53:00 +0100

    Create dictionary.ml

---
+++ ds/dictionary/dictionary.ml
@@ -0,0 +1,24 @@
+type ('a, 'b) dictionary = {hash: 'a -> int; datas: ('a * 'b) list array; width: int};;
+
+(** [create h w] creates a dictionary of size w with a hash table h *)
+let create h w =
+    {hash = h; datas = Array.make w []; width = w}
+
+(** [search t k] returns if there is a key k or not in a dictionary t *)
+let search t k =
+    t.datas.(t.hash k)
+    |> List.map fst
+    |> List.mem k
+
+(** [element t k] return the value associated with the key k in a dictionnary t, if the key exists *)
+let element t k =
+    List.filter (fun c -> fst c = k) t.datas.(t.hash k)
+    |> List.hd
+
+(** [add t k e] adds an element e with the key k in a dictionary t *)
+let add t k e =
+    t.datas(t.hash k) <- (k, e)::t.datas(t.hash k)
+
+(** [delete t k] deletes the element associated with the key k in a dictionary t, if the key exists *)
+let delete t k =
+    t.datas(t.hash k) <- List.filter (fun c -> fst c <> k) t.datas.(t.hash k)
