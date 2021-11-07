ed3d53bd7fe4caf09a6fa9ef5addd28bfb9e9600
ChloeLucas1 <90566955+ChloeLucas1@users.noreply.github.com>
2021-11-07 20:48:11 +0100

    Update list.ml

--- ds/list/list.ml
+++ ds/list/list.ml
@@ -2,3 +2,21 @@
 let rec sum l = match l with
     | [] -> 0
     | e::q -> e + sum q;;
+
+(** [find x l] search an element x in a list l *)
+let rec find x l = match l with
+    | [] -> false
+    | e::q -> e = x || find x q
+
+(** [reverse l] reverse a list l *)
+let reverse l =
+    let rec rev l acc = match l with
+        | [] -> acc
+        | e::q -> rev q (e::acc) in
+    rev l []
+
+(** [increasing l] return if the list is increasing or not *)
+let rec increasing l = match l with
+    | [] -> true
+    | [e] -> true
+    | e1::e2::q -> e1 <= e2 && increasing (e2::q)
