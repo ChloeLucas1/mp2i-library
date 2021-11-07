66e381f1ec1a8d8bc5bd704bef7d5a72c18def19
ChloeLucas1 <90566955+ChloeLucas1@users.noreply.github.com>
2021-11-07 18:50:07 +0100

    Update duplicate.ml

--- algo/duplicate/duplicate.ml
+++ algo/duplicate/duplicate.ml
@@ -3,5 +3,5 @@ let duplicate l =
     let rec aux = function
         | [] -> false
         | [e] -> false
-        | e1::e2::q -> e1 = e2 || aux e2::q in
+        | e1::e2::q -> e1 = e2 || aux (e2::q) in
     aux (tri l)
