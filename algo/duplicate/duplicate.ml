ebf0c213e92670fe03989b994e1faad5c41de38e
ChloeLucas1 <90566955+ChloeLucas1@users.noreply.github.com>
2021-11-07 18:35:14 +0100

    Create duplicate.ml

---
+++ algo/duplicate/duplicate.ml
@@ -0,0 +1,6 @@
+(** [duplicate l] return if there is a duplicate (two identical elements) in a list l *)
+let duplicate l =
+    let rec aux = function
+        | [] | [e] -> false
+        | e1::e2::q -> e1 = e2 || aux e2::q in
+    aux (tri l)
