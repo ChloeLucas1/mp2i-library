74c3516db5d0fda9c60c704c95b6ce10e3f77194
ChloeLucas1 <90566955+ChloeLucas1@users.noreply.github.com>
2021-11-07 18:54:04 +0100

    Update duplicate.ml

--- algo/duplicate/duplicate.ml
+++ algo/duplicate/duplicate.ml
@@ -1,7 +1,9 @@
+#use "sort.ml"
+
 (** [duplicate l] return if there is a duplicate (two identical elements) in a list l *)
 let duplicate l =
     let rec aux = function
         | [] -> false
         | [e] -> false
         | e1::e2::q -> e1 = e2 || aux (e2::q) in
-    aux (tri l)
+    aux (merge_sort l)
